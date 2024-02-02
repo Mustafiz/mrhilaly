a23904ef09c998a0a64d7cf9924ca0e9d07cf003
Mustafiz <hilaly.mustafiz@hotmail.com>
2024-02-01 18:01:13 -0600

    Change the content of script.js.

--- scripts/script.js
+++ scripts/script.js
@@ -28,7 +28,7 @@ document.getElementById('contact-form').addEventListener('submit', function (eve
 
 function submitForm(formData) {
     // Make an API request to the backend (API Gateway) for form submission
-    fetch('https://api.mustafizrh.net/dev/submit', { // URL that represents the backend API endpoint to which the form data is going to be sent
+    fetch('https://y0upmnywf9.execute-api.us-east-1.amazonaws.com/dev/submit', { // URL that represents the backend API endpoint to which the form data is going to be sent
         method: 'POST',
         mode: "cors",
         headers: {
